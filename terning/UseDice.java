9371463c39b2607ccb4ab21c96aeb42597fa0e34
Shadow <lasse@dyrsted.dk>
2016-09-27 07:18:36 +0200

    Input exception handle
    Programmet tager nu kun imod integers som input.

--- terning/UseDice.java
+++ terning/UseDice.java
@@ -20,13 +20,29 @@ import java.util.Scanner;
 
 public class UseDice {
 
-	public static void main(String[] args) {
-		Dice cup = new Dice();   // Make an instance of Dice
-		Scanner keyboard=new Scanner(System.in);   // Make an instance of keyboard Scanner
-		System.out.println("Welcome to the diceroll program");
-		System.out.println("How many times do you want to roll the dice?");
-		int n = keyboard.nextInt();  // Read integer
-		cup.rollMultiple(n);         // Roll the dice n times
-		keyboard.close();            // Close the scanner
-	}
+    public static void main(String[] args) {
+        int n = 0;
+        boolean isOk = false;
+
+        Dice cup = new Dice();   // Make an instance of Dice
+        Scanner keyboard = new Scanner(System.in);   // Make an instance of keyboard Scanner
+        System.out.println("Welcome to the diceroll program");
+        System.out.println("How many times do you want to roll the dice?");
+
+        while (!isOk) {
+            try {
+                n = keyboard.nextInt();
+                isOk = true;
+            } catch (Exception e) {
+
+                //Når inputtet er ulovligt fjernes den ulovlige indtastning.
+                keyboard.nextLine();
+
+                //Brugeren bliver gjort opmærksom på han laver noget forkert.
+                System.out.println("Indtastning skal være af typen Integer, prøv igen.");
+            }
+        }
+        cup.rollMultiple(n);         // Roll the dice n times
+        keyboard.close();            // Close the scanner
+    }
 }
